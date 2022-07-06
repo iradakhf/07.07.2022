ddf73394b0ba2206597730d5373f84b91c931716
iradakhf <iradakhf@code.edu.az>
2022-07-06 23:14:07 +0400

    add 1st

---
+++ Cat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Cat
+    {
+        public string name;
+        public string color;
+        public int age;
+
+        public Cat(string name, string color, int age)
+        {
+            this.name = name;
+            this.color = color;
+            this.age = age;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp1;
+using System;
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            Dog dog = new Dog("labrador", "golden", 2);
+            Cat cat = new Cat("anyType", "black", 1);
+
+            Console.WriteLine(dog.name);
+            Console.WriteLine(cat.color);
+
+        }
+       
+        }
+    }
